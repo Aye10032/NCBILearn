4ad98a624df6b997eae12f6c731e9a40766506d9
Aye10032 <aye10032@gmail.com>
2023-10-08 09:38:47 +0800

    update

--- FindArticleByAU.py
+++ FindArticleByAU.py
@@ -1,15 +1,17 @@
 import requests
-from Bio import Entrez, SeqIO
+from Bio import Entrez, SeqIO, Medline
 from bs4 import BeautifulSoup
 
-article_list = []
+pmc_list = []
+pm_list = []
 # 设置Entrez参数
 Entrez.email = 'aye10032@gmail.com'
 db = 'PMC'
 
 
 def get_articles():
-    global article_list
+    global pmc_list, pm_list
+
     search_term = ('(Sun, Luyang[Full Author Name]) AND '
                    '('
                    '(Auburn University[Affiliation]) OR '
@@ -20,19 +22,30 @@ def get_articles():
 
     handle = Entrez.esearch(db=db, term=search_term)
     record = Entrez.read(handle)
+    handle.close()
 
-    article_list = record['IdList']
+    pmc_list = record['IdList']
 
-    print(article_list)
+    with open('output.txt', 'a') as _file:
+        for pmc in pmc_list:
+            handle = Entrez.efetch(db=db, id=pmc, rettype='medline', retmode='text')
+            article = Medline.read(handle)
+            handle.close()
+
+            pm_list.append(article['PMID'])
+            _file.write(article['TI'] + '\n')
+            _file.write(f'PMC:{pmc}\n')
+            _file.write('PubMed:' + article['PMID'] + '\n')
+            _file.write(article['AID'][0] + '\n\n')
 
 
 def get_seq_database():
-    global article_list
+    global pmc_list
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
-    for pmc_id in article_list:
+    for pmc_id in pmc_list:
         url = f'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC{pmc_id}/'
         print(url)
 
@@ -54,5 +67,9 @@ def get_seq_database():
         print()
 
 
-get_articles()
-get_seq_database()
+if __name__ == '__main__':
+    with open('output.txt', 'a') as file:
+        file.write('过去10年内发表文章:\n\n')
+
+    get_articles()
+    get_seq_database()
